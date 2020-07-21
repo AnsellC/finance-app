14e20e031852121a2e29f609c13ccbe92e305c5b
AnsellC <ansellcruz@gmail.com>
2020-07-22 04:49:11 +0800

    add vuex

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -3,10 +3,12 @@ import axios from 'axios';
 import App from '@/App.vue';
 import router from '@/router';
 import vuetify from '@/plugins/vuetify';
+import store from '@/vuex';
 
 Vue.prototype.$axios = axios;
 const app = new Vue({
     router,
+    store,
     vuetify,
     render: h => h(App)
 }).$mount('#app');
---
+++ resources/js/vuex.ts
@@ -0,0 +1,14 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+const baseState = {};
+
+export default new Vuex.Store({
+    state: baseState,
+    getters: {},
+    mutations: {},
+    actions: {},
+    modules: {}
+});
