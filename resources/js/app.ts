77aa7be51c4f79f8f06e1f367aa758294670b243
AnsellC <ansellcruz@gmail.com>
2020-07-22 04:45:01 +0800

    add router

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,10 +1,12 @@
 import Vue from 'vue';
 import axios from 'axios';
-import App from './App.vue';
-import vuetify from './plugins/vuetify';
+import App from '@/App.vue';
+import router from '@/router';
+import vuetify from '@/plugins/vuetify';
 
 Vue.prototype.$axios = axios;
 const app = new Vue({
+    router,
     vuetify,
     render: h => h(App)
 }).$mount('#app');
