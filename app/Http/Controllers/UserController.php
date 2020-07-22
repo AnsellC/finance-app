846e680a495f021299a0c90e83ab9e38cdedaa2b
AnsellC <ansellcruz@gmail.com>
2020-07-22 23:25:04 +0800

    hash passwords

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,15 +4,19 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\CreateUserRequest;
 use App\User;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function create(CreateUserRequest $request)
     {
         $validated = $request->validated();
-        $user = User::create($validated);
-        
+        $user = User::create([
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'password' => Hash::make($validated['password']),
+        ]);
+
         return response()->json($user, 201);
     }
 }
