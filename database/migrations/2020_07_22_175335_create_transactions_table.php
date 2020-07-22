62830ca1954c0511dc68fb9031959ae387870c8f
AnsellC <ansellcruz@gmail.com>
2020-07-23 03:33:00 +0800

    create migration for transactions table

---
+++ database/migrations/2020_07_22_175335_create_transactions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id');
+            $table->string('label');
+            $table->timestampTz('date');
+            $table->float('amount');
+            $table->enum('type', ['credit', 'debit'])->default('credit');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
