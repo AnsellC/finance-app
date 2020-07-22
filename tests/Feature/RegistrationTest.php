3c2941add550f059a88a3f5763c6acf2ce020a01
AnsellC <ansellcruz@gmail.com>
2020-07-22 23:29:15 +0800

    apply php-cs

--- tests/Feature/RegistrationTest.php
+++ tests/Feature/RegistrationTest.php
@@ -3,12 +3,12 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class RegistrationTest extends TestCase
 {
     use RefreshDatabase;
+
     /** @test */
     public function a_user_can_register()
     {
@@ -16,11 +16,10 @@ class RegistrationTest extends TestCase
             'name' => 'John Doe',
             'email' => 'email@email.com',
             'password' => '12345678',
-            'password_confirmation' => '12345678'
+            'password_confirmation' => '12345678',
         ];
 
         $result = $this->postJson('/api/register', $userData);
-
 
         $result->assertStatus(201);
     }
