d7f466a2635111bbedcc0e85e766b391dd4af527
AnsellC <ansellcruz@gmail.com>
2020-07-22 23:24:54 +0800

    add unique attribute to email

--- app/Http/Requests/CreateUserRequest.php
+++ app/Http/Requests/CreateUserRequest.php
@@ -25,8 +25,8 @@ class CreateUserRequest extends FormRequest
     {
         return [
             'name' => 'required|min:5|max:50',
-            'email' => 'required|email',
-            'password' => 'required|confirmed|min:8'
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed|min:8',
         ];
     }
 }
