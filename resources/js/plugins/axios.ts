be39676b986228f26047a6319095a6ba9eec26d2
AnsellC <ansellcruz@gmail.com>
2020-07-23 01:00:40 +0800

    refactor axios

---
+++ resources/js/plugins/axios.ts
@@ -0,0 +1,7 @@
+import Vue from "vue";
+import axios from "axios";
+import config from "@/config";
+
+axios.defaults.baseURL = config.endpoints.baseURL;
+axios.defaults.headers.common.Authorization = '';
+export default axios;
