3cab9e491a7c086990359ff4b1a1bfb008916aed
AnsellC <ansellcruz@gmail.com>
2020-07-23 00:59:56 +0800

    add auth check

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,13 +1,16 @@
 import Vue from 'vue';
-import axios from 'axios';
+import axios from '@/plugins/axios';
 import App from '@/App.vue';
 import router from '@/router';
 import vuetify from '@/plugins/vuetify';
 import store from '@/vuex';
 import * as _ from 'lodash'
+import User from "@/models/user";
+import { UserPlugin } from "@/plugins/user";
+import config from "@/config";
 
 Vue.prototype.$axios = axios;
-Vue.prototype.$axios.defaults.baseURL = '/api';
+Vue.use(UserPlugin);
 
 // Automatic component registration
 const requireComponent = require.context(
@@ -30,22 +33,42 @@ requireComponent.keys().forEach(fileName => {
     Vue.component(componentName, componentConfig.default || componentConfig);
 });
 
-const isLoggedIn = store.getters['isLoggedIn'];
-
-router.beforeEach((to, from, next) => {
-    const requiresAuth = to.matched.some(x => x.meta.requiresAuth);
-    Vue.prototype.$isSinglePage = to.matched.some(x => x.meta.isSinglePage);
-
-    if (requiresAuth && !isLoggedIn) {
-        next('/login');
-    } else {
-        next();
+(async () => {
+    // Check if browser has token stored.
+    const storageToken = localStorage.getItem(config.localStorageKey)
+    if (storageToken) {
+        const auth = JSON.parse(storageToken);
+        try {
+            await Vue.prototype.$user.me(auth.token, auth.expires);
+        }
+        catch(error) {
+            localStorage.removeItem(config.localStorageKey);
+        }
     }
-});
+})().then(() => {
+    const isLoggedIn = store.getters['isLoggedIn'];
 
-const app = new Vue({
-    router,
-    store,
-    vuetify,
-    render: h => h(App)
-}).$mount('#app');
+    router.beforeEach((to, from, next) => {
+        const requiresAuth = to.matched.some(x => x.meta.requiresAuth);
+        const requiresGuest = to.matched.some(x => x.meta.requiresGuest);
+        Vue.prototype.$isSinglePage = to.matched.some(x => x.meta.isSinglePage);
+
+        if (requiresAuth && !isLoggedIn) {
+            next('/login');
+        } else if (requiresGuest && isLoggedIn) {
+            next('/dashboard');
+        } else if (to.path === '/') {
+            next('/dashboard');
+        } else {
+            next();
+        }
+    });
+
+    const app = new Vue({
+        router,
+        store,
+        vuetify,
+        render: h => h(App)
+    }).$mount('#app');
+
+})
